eba7c71c9c4400d0e617cddf0369961498fa4506
saifsiddiqui528 <saifsiddiqui528@gmail.com>
2026-01-06 12:36:36 +0500

    all

---
+++ app.py
@@ -0,0 +1,14 @@
+# print ("Hello")
+# print ("\tHello this is Saifullah, \nWe are learning in web dev with python batch 16, \nI love to coding")
+# 
+# hello=12
+# 
+# print("hello is equal to:", hello)
+
+# num1=50
+# num2=100
+# print("This is Addition", num1+num2, "\nThis is Subtraction", num1-num2, "\nThis is Multiply", num1*num2, "\nThis is Divide", num1/num2)
+
+num1=int(input("Enter a number:"))
+
+print ("This is num1:" , num1*num1)
