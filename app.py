a7407a99a88c2dce6059dafa5c158025325506b7
Saifullah <161806398+saifsiddiqui528@users.noreply.github.com>
2026-01-07 12:18:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,19 @@
 num1=int(input("Enter a number:"))
 
 print ("This is num1:" , num1*num1)
+
+
+fname=input("Enter your first name:")
+lname=input("Enter your last name:")
+birthyear=int(input("Enter your birth year:"))
+Email=input("Enter your email:")
+contact=input("Enter your Contact:")
+
+
+age=2026-birthyear
+
+
+print("\t****This is your information*****\n", "Your fullname:", fname +" "+ lname, "\n your age as per today:" , age, "\n your email:", Email, "\n this is your contact:", contact)
+
+
+print(f"This is your full name: {fname+lname}")
